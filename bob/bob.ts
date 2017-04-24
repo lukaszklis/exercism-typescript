4f8f10aeb6e24068c9f634346e7c66f729b28159
Lukasz Klis <lukasz@klis.pl>
2017-04-24 15:28:09 +0200

    :sparkles: Add the ”Bob” task

---
+++ bob/bob.ts
@@ -0,0 +1,17 @@
+export default class Bob {
+    public hey(message: string): string {
+        if (message.trim() === '') {
+            return 'Fine. Be that way!';
+        }
+
+        if (message === message.toUpperCase() && message.match(/[a-zA-Z]/g)) {
+            return 'Whoa, chill out!';
+        }
+
+        if (message.endsWith('?')) {
+            return 'Sure.';
+        }
+
+        return 'Whatever.';
+    }
+}
