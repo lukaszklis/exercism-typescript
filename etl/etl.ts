c38e1c189a5c37914a673a7a6d657f49a2dc0dcc
Lukasz Klis <lukasz@klis.pl>
2017-05-13 12:48:43 +0200

    :hammer: Use `reduce` instead of `forEach`

--- etl/etl.ts
+++ etl/etl.ts
@@ -7,9 +7,8 @@ interface NewValue {
 }
 
 export default function transform(dataSet: OldValue): NewValue {
-    const newValue: NewValue = {};
-
-    Object.keys(dataSet).forEach((points) => dataSet[points].forEach((letter) => newValue[letter.toLowerCase()] = parseInt(points, 10)));
-
-    return newValue;
+    return Object.keys(dataSet).reduce((newValue: NewValue, score) => {
+        dataSet[score].forEach((letter) => newValue[letter.toLowerCase()] = parseInt(score, 10));
+        return newValue;
+    }, {});
 };
