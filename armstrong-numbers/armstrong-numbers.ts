a7df38541758d6e3781156d2bc62f7b2a07e7078
Lukasz Klis <lukasz@klis.pl>
2018-06-22 16:46:14 +0200

    :zap: Implement the "Armstrong Number" task

---
+++ armstrong-numbers/armstrong-numbers.ts
@@ -0,0 +1,8 @@
+export default class ArmstrongNumbers {
+    public static isArmstrongNumber(input: number): boolean {
+        const digits = `${input}`.split('').map((digit) => parseInt(digit, 10))
+        const factor = digits.length
+
+        return input === digits.reduce((sum, digit) => sum + Math.pow(digit, factor), 0)
+    }
+}
