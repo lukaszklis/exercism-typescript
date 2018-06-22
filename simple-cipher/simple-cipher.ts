4626b5306385b0688ce0517672f4bdbf9b42044d
Lukasz Klis <lukasz@klis.pl>
2018-06-22 16:31:36 +0200

    :zap: Implement the "Simple Cipher" task

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,10 +1,54 @@
+const keyLength = 100
+const minCharCode = 97
+const maxCharCode = 122
+const alphabetLength = 26
+
 class SimpleCipher {
-    encode( /* Parameters go here */ ) {
-        // Your code here
+    public key: string
+
+    constructor(key?: string) {
+        if (key !== undefined && !/[a-z]/.test(key)) {
+            throw new Error('Bad key')
+        }
+
+        this.key = key || this.getRandomKey()
     }
 
-    decode( /* Parameters go here */ ) {
-        // Your code here
+    public encode(input: string): string {
+        return input
+            .split('')
+            .map((_, i) => {
+                let encodedCharCode: number = input.charCodeAt(i) + (this.key.charCodeAt(i % this.key.length) - minCharCode)
+
+                if (encodedCharCode > maxCharCode) {
+                    encodedCharCode -= alphabetLength
+                }
+
+                return String.fromCharCode(encodedCharCode)
+            })
+            .join('')
+    }
+
+    public decode(input: string): string {
+        return input
+            .split('')
+            .map((_, i) => {
+                let encodedCharCode: number = input.charCodeAt(i) - (this.key.charCodeAt(i % this.key.length) - minCharCode)
+
+                if (encodedCharCode < minCharCode) {
+                    encodedCharCode += alphabetLength
+                }
+
+                return String.fromCharCode(encodedCharCode)
+            })
+            .join('')
+    }
+
+    private getRandomKey(): string {
+        return [...Array(keyLength)]
+            .map(() => Math.floor(Math.random() * alphabetLength) + minCharCode)
+            .map((charCode) => String.fromCharCode(charCode))
+            .join('')
     }
 }
 
