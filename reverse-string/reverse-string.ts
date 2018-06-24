ece5b843857755b12e5e49dea03547a3760ca130
Lukasz Klis <lukasz@klis.pl>
2018-06-24 12:05:42 +0200

    :zap: Implement the "Reverse String" task

--- reverse-string/reverse-string.ts
+++ reverse-string/reverse-string.ts
@@ -1,7 +1,5 @@
-class ReverseString {
-    static reverse( /* Parameters go here */ ) {
-        // Your code here
+export default class ReverseString {
+    public static reverse(input: string): string {
+        return input.split('').reverse().join('')
     }
 }
-
-export default ReverseString
