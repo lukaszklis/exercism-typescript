1646f8e62b46a22b40478a7207ec297899dc1ebd
Lukasz Klis <lukasz@klis.pl>
2018-07-18 17:37:27 +0200

    :zap: Implement the "Matrix" task (#1)

--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,5 +1,38 @@
 class Matrix {
-    constructor() {
+    private matrixString: string
+
+    constructor(input: string) {
+        this.matrixString = input
+    }
+
+    get rows(): number[][] {
+        return this.getRowsMatrix()
+    }
+
+    get columns(): number[][] {
+        return this.getColumnsMatrix()
+    }
+
+    private getRowsMatrix(): number[][] {
+        return this.matrixString
+            .split('\n')
+            .map((row) => row.split(' ').map(Number))
+    }
+
+    private getColumnsMatrix(): number[][] {
+        const columnsMatrix: number[][] = []
+
+        for (let i = 0; i < this.rows.length; i++) {
+            for (let j = 0; j < this.rows[i].length; j++) {
+                if (!columnsMatrix[j]) {
+                    columnsMatrix[j] = []
+                }
+
+                columnsMatrix[j][i] = this.rows[i][j]
+            }
+        }
+
+        return columnsMatrix
     }
 }
 
